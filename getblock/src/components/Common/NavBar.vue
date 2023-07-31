3485768638d29f33c6843f516d42495570f0c612
Leewang31 <kiwan1111@gmail.com>
2023-07-31 14:06:33 +0900

    feat/lkw/nav UI finish

--- getblock/src/components/Common/NavBar.vue
+++ getblock/src/components/Common/NavBar.vue
@@ -3,20 +3,25 @@
     <div class="navBarWrapper">
       <Logo/>
       <nav>
-        <ul class="navBarUl">
+        <ul class="navBarUl" @mouseover="isMouseOver=true">
           <li class="navBarLi" :class="{navBorderBottom:isNavMouseOver[0]}" @mouseover="mouseOver(0)"
               @mouseout="mouseOut(0)">Company
           </li>
-          <li class="navBarLi" :class="{navBorderBottom:isNavMouseOver[1]}" @mouseover="mouseOver(1)" @mouseout="mouseOut(1)">Products</li>
-          <li class="navBarLi" :class="{navBorderBottom:isNavMouseOver[2]}" @mouseover="mouseOver(2)" @mouseout="mouseOut(2)">Patent</li>
-          <li class="navBarLi" :class="{navBorderBottom:isNavMouseOver[3]}" @mouseover="mouseOver(3)" @mouseout="mouseOut(3)">Application</li>
-          <li class="navBarLi" :class="{navBorderBottom:isNavMouseOver[4]}" @mouseover="mouseOver(4)" @mouseout="mouseOut(4)">Inquiry</li>
+          <li class="navBarLi" :class="{navBorderBottom:isNavMouseOver[1]}" @mouseover="mouseOver(1)"
+              @mouseout="mouseOut(1)">Products
+          </li>
+          <li class="navBarLi" :class="{navBorderBottom:isNavMouseOver[2]}" @mouseover="mouseOver(2)"
+              @mouseout="mouseOut(2)">Application
+          </li>
+          <li class="navBarLi" :class="{navBorderBottom:isNavMouseOver[3]}" @mouseover="mouseOver(3)"
+              @mouseout="mouseOut(3)">Inquiry
+          </li>
         </ul>
       </nav>
       <div class="divBlank"/>
     </div>
   </header>
-  <NavModal :isMouseOver="isMouseOver"/>
+  <NavModal :isMouseOver="isMouseOver" @closeModal="isMouseOver=$event"/>
 </template>
 
 <script>
@@ -30,16 +35,15 @@ export default {
   data() {
     return {
       isMouseOver: false,
-      isNavMouseOver: [false, false, false, false, false]
+      isNavMouseOver: [false, false, false, false],
+      clickNum: 0,
     }
   },
   methods: {
     mouseOver(i) {
-      this.isMouseOver = true;
       this.isNavMouseOver[i] = true
     },
     mouseOut(i) {
-      this.isMouseOver = false;
       this.isNavMouseOver[i] = false
     }
   }
--- getblock/src/components/Common/NavModal.vue
+++ getblock/src/components/Common/NavModal.vue
@@ -1,51 +1,76 @@
 <template>
   <transition name="modal">
-    <div class="modal" v-if="isMouseOver"></div>
+    <div class="modal" v-if="isMouseOver" @mouseleave="$emit('closeModal',false)">
+      <div class="navModalWrapper" >
+        <ul class="navModalDetailUl" >
+          <li class="navModalDetailLi">
+            <p @mouseover="mouseOver(0,0)" @mouseout="mouseOut(0,0)"
+               :class="{navDetailBorderBottom:isNavDetailMouseOver[0][0]}">Introduction</p>
+          </li>
+          <li class="navModalDetailLi"><p @mouseover="mouseOver(0,1)" @mouseout="mouseOut(0,1)"
+                                          :class="{navDetailBorderBottom:isNavDetailMouseOver[0][1]}">History</p></li>
+          <li class="navModalDetailLi"><p @mouseover="mouseOver(0,2)" @mouseout="mouseOut(0,2)"
+                                          :class="{navDetailBorderBottom:isNavDetailMouseOver[0][2]}">Patent</p></li>
+          <li class="navModalDetailLi"><p @mouseover="mouseOver(0,3)" @mouseout="mouseOut(0,3)"
+                                          :class="{navDetailBorderBottom:isNavDetailMouseOver[0][3]}">Organization</p>
+          </li>
+        </ul>
+        <ul class="navModalDetailUl">
+          <li class="navModalDetailLi"><p @mouseover="mouseOver(1,0)" @mouseout="mouseOut(1,0)"
+                                          :class="{navDetailBorderBottom:isNavDetailMouseOver[1][0]}">
+            ChaRacteristics</p>
+          </li>
+          <li class="navModalDetailLi"><p @mouseover="mouseOver(1,1)" @mouseout="mouseOut(1,1)"
+                                          :class="{navDetailBorderBottom:isNavDetailMouseOver[1][1]}">Difference</p>
+          </li>
+          <li class="navModalDetailLi"><p @mouseover="mouseOver(1,2)" @mouseout="mouseOut(1,2)"
+                                          :class="{navDetailBorderBottom:isNavDetailMouseOver[1][2]}">Test &
+            Certification</p></li>
+        </ul>
+        <ul class="navModalDetailUl">
+          <li class="navModalDetailLi">
+            <p @mouseover="mouseOver(2,0)" @mouseout="mouseOut(2,0)"
+               :class="{navDetailBorderBottom:isNavDetailMouseOver[2][0]}">Way To Apply</p></li>
+          <li class="navModalDetailLi"><p @mouseover="mouseOver(2,1)" @mouseout="mouseOut(2,1)"
+                                          :class="{navDetailBorderBottom:isNavDetailMouseOver[2][1]}">Applications</p>
+          </li>
+        </ul>
+        <ul class="navModalDetailUl">
+          <li class="navModalDetailLi"><p @mouseover="mouseOver(3,0)" @mouseout="mouseOut(3,0)"
+                                          :class="{navDetailBorderBottom:isNavDetailMouseOver[3][0]}">Inquiry</p></li>
+        </ul>
+      </div>
+    </div>
   </transition>
 </template>
 
 <script>
+import './NavModal.css'
+
 export default {
   name: "NavModal",
   props: {
     isMouseOver: Boolean,
+  },
+  data() {
+    return {
+      isNavDetailMouseOver: [
+        [false, false, false, false],
+        [false, false, false, false],
+        [false, false, false, false],
+        [false, false, false, false]]
+    }
+  },
+  methods: {
+    mouseOver(i, j) {
+      this.isNavDetailMouseOver[i][j] = true;
+    },
+    mouseOut(i, j) {
+      this.isNavDetailMouseOver[i][j] = false;
+    }
   }
 }
 </script>
 
 <style scoped>
-.modal {
-  position: fixed;
-  left: 0;
-  top: 80px;
-  width: 100%;
-  height: 300px;
-  background: var(--lightblack);
-  box-shadow: 0 3px 8px -1px var(--lightblack);
-}
-
-.modal-enter-from {
-  height: 0
-}
-
-.modal-enter-active {
-  transition: all 0.4s
-}
-
-.modal-enter-to {
-  height: 300px;
-}
-
-.modal-leave-from {
-  height: 300px;
-}
-
-.modal-leave-active {
-  transition: all 0.4s
-}
-
-.modal-leave-to {
-  height: 0px;
-}
-
 </style>
