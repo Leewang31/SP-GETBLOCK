3a12a89e33aeb62817388742d6284b2a14cb38ff
LeeWang <99003659+Leewang31@users.noreply.github.com>
2023-08-01 18:17:32 +0900

    feat/lkw/footer

--- getblock/src/App.vue
+++ getblock/src/App.vue
@@ -1,20 +1,26 @@
 <template>
   <NavBar/>
-  <router-view></router-view>
+  <router-view class="mb"></router-view>
+  <Footer/>
 </template>
 
 <script>
 
 import NavBar from "@/components/Common/NavBar";
+import Footer from "@/components/Common/Footer";
 
 export default {
   name: 'App',
   components: {
+    Footer,
     NavBar,
+
   }
 }
 </script>
 
 <style>
-
+.mb{
+  margin-bottom: 160px;
+}
 </style>
--- getblock/src/components/Common/Footer.vue
+++ getblock/src/components/Common/Footer.vue
@@ -1,5 +1,14 @@
 <template>
-<div></div>
+  <div class="footerWrapper">
+    <div class="footerContent">
+    <p class="logoDiv">GETBLOCK™</p>
+      <div>TEL: ++ 82 70 7601 9672 FAX: ++ 82 32 234 4672 CONTACTS: sales@getblock.com ADDRESS: #Na-211, Incheon
+        Techpia, 223-38 Seoknam-dong. Seo-gi
+        Incheon city, Korea.
+        Copyright (c) 2012 JINSUNG Corporation. All Rights Reserved
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +18,29 @@ export default {
 </script>
 
 <style scoped>
+.footerWrapper {
+  background-color: var(--lightgrey);
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+  padding: 54px 0 50px;
+}
 
+.footerContent {
+  width: 800px;
+  display: flex;
+  height: 16px;
+  font-size: 12px;
+  font-family: navLi;
+  justify-content: center;
+
+}
+
+.logoDiv {
+  color: var(--lightblack);
+  cursor: pointer;
+  font-family: logo;
+  font-size: 18px;
+  margin-right: 20px;
+}
 </style>
