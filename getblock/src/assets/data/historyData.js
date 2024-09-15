daff8f54639b3fa11ac85574955756ec1041e57c
neck <neck@famppy.com>
2024-09-15 15:27:47 +0900

    초기화

--- getblock/src/assets/data/historyData.js
+++
@@ -1,9 +0,0 @@
-export default [
-    {year:'2007',month:'12',content:'Start development of GETBLOCK (100% waterproof zipper)'},
-    {year:'2009',month:'01',content:'Established JINSUNG Corporation'},
-    {year:'2010',month:'04',content:'Developed Prototype product'},
-    {year:'2010',month:'10',content:'Registered trademark of GETBLOCK (in Korea, no. 40-0839211)'},
-    {year:'2011',month:'04',content:'Developed Final product (Material, Structure). Applied for a patent'},
-    {year:'2012',month:'01',content:'Obtained a patent (in Korea, no. 10-1103652)'},
-    {year:'2012',month:'04',content:'Applied PCT to get patent(s) overseas'},
-]
